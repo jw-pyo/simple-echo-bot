0d4285751a9bfff028bfbd67e9d05d53a341ae3c
hyeonwoong <hyeonwoong@comcom.ai>
2022-02-07 17:08:36 +0900

    feat: add echo bot server

---
+++ util.js
@@ -0,0 +1,25 @@
+const parsePath = (path) => {
+  if (!path) {
+    return [];
+  }
+
+  return path.split('/').filter((node) => {
+    return !!node;
+  })
+}
+
+const formatPath = (parsedPath) => {
+  if (!Array.isArray(parsedPath) || parsedPath.length === 0) {
+    return '/';
+  }
+  let formatted = '';
+  for (const label of parsedPath) {
+    formatted += '/' + String(label);
+  }
+  return (formatted.startsWith('/') ? '' : '/') + formatted;
+}
+
+module.exports = {
+  parsePath,
+  formatPath,
+}
